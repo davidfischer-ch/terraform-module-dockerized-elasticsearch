fe2fe0f622a45a9e0fd0041f2c5258eccd642cce
David Fischer <david@fisch3r.net>
2026-03-14 00:31:37 +0100

    Release v1.2.0

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -9,7 +9,7 @@ resource "docker_network" "app" {
 }
 
 module "elasticsearch" {
-  source = "git::https://github.com/davidfischer-ch/terraform-module-dockerized-elasticsearch.git?ref=1.1.0"
+  source = "git::https://github.com/davidfischer-ch/terraform-module-dockerized-elasticsearch.git?ref=1.2.0"
 
   identifier = "my-app-search"
   image_id   = docker_image.elasticsearch.image_id
